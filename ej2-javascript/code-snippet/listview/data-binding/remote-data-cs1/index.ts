d729948233bb2fe6999a0efca1064c53caa793e0
Muthukrishnan Kandasamy <muthukrishnan.kandasamy@syncfusion.com>
2023-12-13 19:01:28 +0530

    documentation(ES-851380): remote data sample issue resolved.

--- ej2-javascript/code-snippet/listview/data-binding/remote-data-cs1/index.ts
+++ ej2-javascript/code-snippet/listview/data-binding/remote-data-cs1/index.ts
@@ -9,23 +9,22 @@ import { DataManager, Query } from '@syncfusion/ej2-data';
 let listviewInstance: ListView = new ListView({
     //bind the DataManager instance to dataSource property
     dataSource: new DataManager({
-        url: '//js.syncfusion.com/ejServices/Wcf/Northwind.svc/',
+        url: 'https://services.syncfusion.com/js/production/api/',
         crossDomain: true
     }),
 
-    //bind the Query instance to query property
-    query: new Query().from('Products').select('ProductID,ProductName').take(6),
+    //Initialize query with the Query instance to get specified set of data
+    query: new Query().from('ListView').select('EmployeeID,FirstName').take(10),
 
-    //map the appropriate columns to fields property
-    fields: { id: 'ProductID', text: 'ProductName' },
+    //Map the appropriate columns to fields property
+    fields: { id: 'EmployeeID', text: 'FirstName' },
 
-    //set the header tittle for the list
-    headerTitle: 'Product Name',
+    //Set header title
+    headerTitle: 'Employees',
+
+    //Set true to show header title
     showHeader: true
 });
 
 //Render initialized ListView
-listviewInstance.appendTo("#element")
-
-
-
+listviewInstance.appendTo("#element");
