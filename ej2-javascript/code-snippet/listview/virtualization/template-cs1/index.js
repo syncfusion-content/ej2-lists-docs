afc94258bf99e590aa1b674bb68a76353b2193ad
Muthukrishnan Kandasamy <muthukrishnan.kandasamy@syncfusion.com>
2023-12-13 15:47:14 +0530

    documentation(ES-851380): Clear the UG preview issues of EJ2 platform

--- ej2-javascript/code-snippet/listview/virtualization/template-cs1/index.js
+++ ej2-javascript/code-snippet/listview/virtualization/template-cs1/index.js
@@ -4,19 +4,29 @@ listData = [
     { name: 'Nancy', icon: 'N', id: '0', },
     { name: 'Andrew', icon: 'A', id: '1' },
     { name: 'Janet', icon: 'J', id: '2' },
-    { name: 'Margaret', imgUrl: './margaret.png', id: '3' },
+    { name: 'Margaret', imgUrl: '//ej2.syncfusion.com/demos/src/listview/images/margaret.png', id: '3' },
     { name: 'Steven', icon: 'S', id: '4' },
-    { name: 'Laura', imgUrl: './laura.png', id: '5' },
+    { name: 'Laura', imgUrl: '//ej2.syncfusion.com/demos/src/listview/images/laura.png', id: '5' },
     { name: 'Robert', icon: 'R', id: '6' },
     { name: 'Michael', icon: 'M', id: '7' },
-    { name: 'Albert', imgUrl: './albert.png', id: '8' },
+    { name: 'Albert', imgUrl: '//ej2.syncfusion.com/demos/src/listview/images/albert.png', id: '8' },
     { name: 'Nolan', icon: 'N', id: '9' }
 ]
 for (var i = 10; i <= 1010; i++) {
     var index = parseInt((Math.random() * 10).toString());
     listData.push({ name: listData[index].name, icon: listData[index].icon, imgUrl: listData[index].imgUrl, id: i.toString() });
 }
-
+var template = function (data) {
+    var showIcon = data.imgUrl ? 'hideUI' : 'showUI';
+    var showImg = data.imgUrl ? 'showUI' : 'hideUI';
+    var imgUrl = data.imgUrl || '';
+    var result = '<div class="e-list-wrapper e-list-avatar">' +
+    '<span class="e-avatar e-avatar-circle ' + data.icon + ' ' + showIcon + '">' + data.icon + '</span>' +
+    '<img class="e-avatar e-avatar-circle ' + showImg + '" src="' + imgUrl + '" />' +
+    '<span class="e-list-content">' + data.name + '</span>' +
+    '</div>';
+    return result;
+};
 var listObj = new ej.lists.ListView({
 
     //Set defined data to dataSource property.
@@ -34,10 +44,11 @@ var listObj = new ej.lists.ListView({
     //Set true to show header title
     showHeader: true,
 
-    //Set defined customized template
-    template: '<div class="list-container"><div id="icon" class="${$imgUrl ? \'img\' : $icon }"><span class="${$imgUrl ? \'hideUI\' : \'showUI\' }"> ${icon}</span> <img class="${$imgUrl ? \'showUI\' : \'hideUI\' }" width = 45 height = 45 src="${$imgUrl ?  $imgUrl : \' \' }" /></div><div class="name">${name}</div></div>'
+    //Set defined customized css class
+    cssClass: 'e-list-template',
 
+    //Set defined customized template
+    template: template
 });
 
 listObj.appendTo('#ui-list');
-
